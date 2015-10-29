7a22261d1dc7b7da66f53a3f4e399b41ad588f4b
Jillian Fetter <jillian.fetter@gmail.com>
2015-10-28 20:49:34 -0700

    randomizes now

--- 10.28.15/nameGame/script.js
+++ 10.28.15/nameGame/script.js
@@ -1,13 +1,28 @@
-var students = [];
-
 document.getElementById('addButton').addEventListener('click', addStudent);
 
+
+var students = [];
+var randomizedStudents = [];
+
+//reuse this function for pairing and teaming
+function moveArrayToDOM(array, id){
+document.getElementById(id).innerHTML = "";
+var containerDiv =[];
+return array.forEach(function(item, index, all){
+var li = document.createElement("li");
+var textItem = document.createTextNode(item);
+li.appendChild(textItem);
+document.getElementById(id).appendChild(li);
+});
+}
+
+
 var studentNameInput = document.getElementById('studentName');
-studentNameInput.addEventListener('keypress', keyPressed)
+studentNameInput.addEventListener('keypress', enterPressed)
 
 studentNameInput.focus();
 
-function keyPressed(event) {
+function enterPressed() {
  if (event.charCode === 13) {
     addStudent();
  }
@@ -18,32 +33,29 @@ function addStudent() {
  if (inputStr) {
  
    var names = inputStr.trim().split(',');
-
    names = names.map(function(name){
        return name.trim();
    }).filter(function(name){
        return name !== "";
    });
-
    students = students.concat(names);
-
-
  studentNameInput.value = '';
  studentNameInput.focus();
 
- var containerDiv =[];
+ moveArrayToDOM(students, "namesUL");
 
-students.forEach(function(item, index, all){
-var div = document.createElement("div");
-var textItem = document.createTextNode(item);
-div.appendChild(textItem);
-console.log("div =" + div);
-document.getElementById("namesDiv").appendChild(div); 
-
-// containerDiv.push(div);
-// ("containerDiv =" + containerDiv);
-// div.setAttribute("class", "name");
-});
  }
 }
+
+document.getElementById('triggerRandomizer').addEventListener('click', randomizeStudents);
+function randomizeStudents(){
+	var randomStudents = students.sort(function(a,b){
+		return 0.5 - Math.random();
+});
+	console.log(students);
+	moveArrayToDOM(randomStudents);
+    
+
+};
+
 
