2fd68904ed0f293245c0159954ac6828944178b0
Jillian Fetter <jillian.fetter@gmail.com>
2015-10-29 21:44:34 -0700

    some basic css to make calculaator look calcish when button pressed will push a string to an array

--- 10.29.15/calculator/main.js
+++ 10.29.15/calculator/main.js
@@ -4,9 +4,30 @@ function init (){
 	console.log("ready");
 }
 
-$("#equal").click(function(){
-	$("#display").text("READY");
+var input = [];
+var numberHolder = [];
 
+// var nine = 
+// var eight =
+// var seven =
+// var six =
+// var five =
+// var four =
+// var three =
+// var two = 
+// var one = 
+// var zero = $("zero").text();
+
+
+$("#equal").on("click", function(array){
+	$("#display").text(array);
+ 		
 });
 
+// only grab numbers until an operator is pushed
+// then push what you've got to a temporary array to operate on)
+$(".numb").on("click", function(keyPressed){
+	var presses = input.push($(this).text());
+	presses = input.join(""));
+});
 
