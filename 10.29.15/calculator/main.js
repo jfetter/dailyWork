6c71884ffc6d4b27d721ca242343b6ba7b04ae4b
Jillian Fetter <jillian.fetter@gmail.com>
2015-10-29 22:26:01 -0700

    buttons will once again console their value and some stuff I'm working on will be commented out

--- 10.29.15/calculator/main.js
+++ 10.29.15/calculator/main.js
@@ -1,33 +1,36 @@
+"use strict"
 $(document).ready(init);
 function init (){
-	"use strict"
-	console.log("ready");
-}
 
 var input = [];
 var numberHolder = [];
-
-// var nine = 
-// var eight =
-// var seven =
-// var six =
-// var five =
-// var four =
-// var three =
-// var two = 
-// var one = 
-// var zero = $("zero").text();
-
 
 $("#equal").on("click", function(array){
 	$("#display").text(array);
  		
 });
 
+
+//create a state operatorPressed
+// let operatorPressed;
+// console.log(operatorPressed)
+
+
+
+
+
+
 // only grab numbers until an operator is pushed
 // then push what you've got to a temporary array to operate on)
-$(".numb").on("click", function(keyPressed){
-	var presses = input.push($(this).text());
-	presses = input.join(""));
+$(".numb").on("click", function(){
+	var presses = $(this).text();
+	input.push(presses);
+	// if (operatorPressed){
+	// presses = parseInt(input.join(""));
+	// numberHolder = numberHolder.concat(input);
+	// input = [];
+	// }
+ 		console.log(presses);
 });
 
+};
