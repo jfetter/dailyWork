5853ddbfeda37412ecef961be6cbd85d8ee8569a
Jillian Fetter <jillian.fetter@gmail.com>
2015-10-29 22:56:48 -0700

    when press operators they will be pushed to an operator array

--- 10.29.15/calculator/main.js
+++ 10.29.15/calculator/main.js
@@ -2,35 +2,50 @@
 $(document).ready(init);
 function init (){
 
-var input = [];
+var numbInput = [];
 var numberHolder = [];
+var operatorHolder = []; 
 
 $("#equal").on("click", function(array){
 	$("#display").text(array);
  		
 });
 
+// let operatorPressed =()=>{
+//  	return true; 
+
+// };
+
 
 //create a state operatorPressed
+
+//define variable operatorPressed;
 // let operatorPressed;
-// console.log(operatorPressed)
+// let $e = $(event.target);
+// $e.text(operatorPressed);
+// $e.data("clicked", operatorPressed);
+// console.log("e +" + $e + "operatorPressed:" + operatorPressed)
+		
 
-
-
+$(".operator").on("click", function(){
+	var opPresses = $(this).text();
+	operatorHolder.push(opPresses);
+	console.log(operatorHolder);
+});
 
 
 
 // only grab numbers until an operator is pushed
 // then push what you've got to a temporary array to operate on)
 $(".numb").on("click", function(){
-	var presses = $(this).text();
-	input.push(presses);
+	var numbPresses = $(this).text();
+	numbInput.push(numbPresses);
 	// if (operatorPressed){
-	// presses = parseInt(input.join(""));
-	// numberHolder = numberHolder.concat(input);
-	// input = [];
+	// numbPresses = parseInt(numbInput.join(""));
+	numberHolder.push();
+	// numbInput = [];
 	// }
- 		console.log(presses);
+ 		console.log(numbInput);
 });
 
 };
